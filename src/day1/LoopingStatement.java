83b0d1094fe3d4b0ec127538b45bc6bbfbf22ed4
OWNER <OWNER@172.16.150.111>
2025-08-06 14:41:12 +0530

    commit

--- src/day1/LoopingStatement.java
+++ src/day1/LoopingStatement.java
@@ -4,7 +4,13 @@ public class LoopingStatement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for(int i= 1; i<=50 ; i++)   
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
 
 	}
 
-}
+
